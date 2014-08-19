1eee32a5611dc762a5ba2790630023d52747c94a
Matt Jenkins <matt@majenko.co.uk>
2014-08-19 17:54:51 +0100

    Updated config bits example

--- examples/ConfigBits/ConfigBits.h
+++ examples/ConfigBits/ConfigBits.h
@@ -5,7 +5,7 @@ struct conf0 {
 	union {
 		uint32_t raw;
 		struct {
-			unsigned debugger:2;
+			unsigned debug:2;
 			#define DEBUGGER_ENABLED 0b10
 			#define DEBUGGER_DISABLED 0b00
 			unsigned :1;
@@ -15,16 +15,72 @@ struct conf0 {
 			unsigned :8;
 			unsigned pwp:8;
 			unsigned :4;
-			unsigned bwp:1
+			unsigned bwp:1;
 			#define BWP_WRITEABLE 0b1
 			#define BWP_PROTECTED 0b0
-			unsigned :3
-			unsigned cp:1
+			unsigned :3;
+			unsigned cp:1;
 			#define CP_ENABLED 0b0
 			#define CP_DISABLED 0b1
-			unsigned :3
+			unsigned :3;
 		};
 	};
+} __attribute__((packed));
+
+struct conf1 {
+    union {
+        uint32_t raw;
+        struct {
+            unsigned fnosc:3;
+            unsigned :2;
+            unsigned fsoscen:1;
+            unsigned :1;
+            unsigned ieso:1;
+            unsigned poscmod:2; 
+            unsigned osciofnc:1;
+            unsigned :1;
+            unsigned fpbdiv:2;
+            unsigned fcksm:2;
+            unsigned wdtps:5;
+            unsigned :2;
+            unsigned fwdten:1;
+            unsigned :8;
+        };
+    };
+} __attribute__((packed));
+
+struct conf2 {
+    union {
+        uint32_t raw;
+        struct {
+            unsigned fpllidiv:3;
+            unsigned :1;
+            unsigned fpllmul:3;
+            unsigned :1;
+            unsigned upllidiv:3;
+            unsigned :4;
+            unsigned upllen:1;
+            unsigned fpllodiv:3;
+            unsigned :13;
+        };
+    };
+} __attribute__((packed));
+
+struct conf3 {
+    union {
+        uint32_t raw;
+        struct {
+            unsigned userid:16;
+            unsigned fsrssel:3;
+            unsigned :5;
+            unsigned fmiien:1;
+            unsigned fethio:1;
+            unsigned fcanio:1;
+            unsigned :3;
+            unsigned fusbidio:1;
+            unsigned fvbusonio:1;
+        };
+    };
 } __attribute__((packed));
 
 #endif
--- examples/ConfigBits/ConfigBits.ino
+++ examples/ConfigBits/ConfigBits.ino
@@ -1,5 +1,8 @@
+#include <ANSI.h>
 #include <Flash.h>
 #include "ConfigBits.h"
+
+ANSI ansi(Serial);
 
 uint32_t *config0addr = (uint32_t *)0xBFC02FFC;
 uint32_t *config1addr = (uint32_t *)0xBFC02FF8;
@@ -65,85 +68,29 @@ const char *ws_ps[32] = {
 	"X", "X", "X", "X", "X", "X", "X"
 };
 
-void displaySettings() {
-	Serial.print("D) Debugger .......................... ");
-	switch (config0.debug) {
-		case 0b00: Serial.println("Enabled"); break;
-		case 0b01: Serial.println("Enabled"); break;
-		case 0b10: Serial.println("Enabled"); break;
-		case 0b11: Serial.println("Disabled"); break;
-	}
-	Serial.print("I) ICE Pins .......................... ");
-	Serial.println(config0.icesel ? "PGE2" : "PGE1");
-	Serial.print("W) Watchdog Timeout .................. ");
-	if (ws_ps[config1.wdtps][0] == 'X') {
-		config1.wdtps = 0b10100;
-	}
-	Serial.println(ws_ps[config1.wdtps]);
-	Serial.print("M) MII Interface ..................... ");
-	Serial.println(config3.fmiien ? "MII" : "RMII");
-	Serial.print("E) Ethernet Pins ..................... ");
-	Serial.println(config3.fethio ? "Default" : "Alternate");
-	Serial.print("C) CAN Pins .......................... ");
-	Serial.println(config3.fcanio ? "Default" : "Alternate");
-	Serial.print("U) USBID Pin.......................... ");
-	Serial.println(config3.fusbidio ? "USBID" : "IO");
-	Serial.print("V) VBUSON Pin......................... ");
-	Serial.println(config3.fvbusonio ? "VBUSON" : "IO");
-}
-
 void setup() {
 	loadSettings();
 	Serial.begin(115200);
 }
 
 void loop() {
+	ansi.cls();
+	ansi.setCursor(0, 0);
+	heading("Main Menu");
 	Serial.println();
-	displaySettings();
+	Serial.println(" D) Debugger");
+	Serial.println(" W) Watchdog");
+	Serial.println(" U) USB");
+	Serial.println(" O) Oscillator");
+	Serial.println(" P) Peripherals");
 	Serial.println();
-	Serial.println("L) Load settings");
-	Serial.println("S) Save settings");
-	Serial.println("R) Reboot");
+	Serial.println(" L) Load settings");
+	Serial.println(" S) Save settings");
+	Serial.println(" R) Reboot");	
 	Serial.println();
-	Serial.print("Select an entry to change: ");
-	while (!Serial.available());
-	char c = Serial.read();
-	if (c >= 'a' && c <= 'z') {
-		c -= 0x20;	
-	}
-	Serial.println(c);
-	switch (c) {
-		case 'D':
-			if (config0.debug == 0b11) {
-				config0.debug = 0b10;
-			} else {
-				config0.debug = 0b11;
-			}
-			break;
-		case 'I':
-			config0.icesel = 1-config0.icesel;
-			break;
-		case 'W':
-			config1.wdtps++;
-			if (config1.wdtps > 0b10100) {
-				config1.wdtps = 0b00000;
-			}
-			break;
-		case 'M':
-			config3.fmiien = 1-config3.fmiien;
-			break;
-		case 'E':
-			config3.fethio = 1-config3.fethio;
-			break;
-		case 'C':
-			config3.fcanio = 1-config3.fcanio;
-			break;
-		case 'U':
-			config3.fusbidio = 1-config3.fusbidio;
-			break;
-		case 'V':
-			config3.fvbusonio = 1-config3.fvbusonio;
-			break;
+	Serial.print("Select an entry: ");
+	char c = getch();
+	switch(c) {
 		case 'L':
 			if (confirm()) {
 				loadSettings();
@@ -163,6 +110,607 @@ void loop() {
 				reboot();
 			}
 			break;
+
+		case 'D':
+			debugger();
+			break;
+		case 'W':
+			watchdog();
+			break;
+		case 'U':
+			usb();
+			break;
+		case 'O':
+			oscillator();
+			break;
+		case 'P':
+			peripherals();
+			break;
 	}
 }
 
+char getch() {
+	while (!Serial.available());
+	char c = Serial.read();
+	if (c >= 'a' && c <= 'z') {
+		c -= 0x20;	
+	}
+	return c;
+}
+
+void subMenuFooter() {
+	Serial.println();
+	Serial.println(" X) Exit to main menu");
+	Serial.println(" L) Load settings");
+	Serial.println(" S) Save settings");
+	Serial.println(" R) Reboot");	
+	Serial.println();
+	Serial.print("Select an entry to change: ");
+}
+
+void heading(char *t) {
+	int len = strlen(t);
+	for (int x = 0; x < 40 - len / 2; x++) {
+		Serial.write(' ');
+	}
+	Serial.println(t);
+	for (int x = 0; x < 80; x++) {
+		Serial.write("-");
+	}
+	Serial.println();
+}
+
+void debugger() {
+	while (1) {
+		ansi.cls();
+		ansi.setCursor(0, 0);
+		heading("Debugger Settings");
+		Serial.println();
+		Serial.print(" D) Debugger .......................... ");
+		switch (config0.debug) {
+			case 0b11: Serial.println("Disabled"); break;
+			default: Serial.println("Enabled"); break;
+		}
+		Serial.print(" I) ICE Pins .......................... ");
+		Serial.println(config0.icesel ? "PGE2" : "PGE1");
+		subMenuFooter();
+		char c = getch();
+		switch(c) {
+			case 'X':
+				return;
+		case 'L':
+			if (confirm()) {
+				loadSettings();
+				Serial.println("Loaded.");
+			}
+			break;
+		case 'S':
+			if (confirm()) {
+				saveSettings();
+				Serial.println("Saved.");
+			}
+			break;
+		case 'R':
+			if (confirm()) {
+				Serial.println("Rebooting...");
+				Serial.flush();
+				reboot();
+			}
+			break;
+		case 'D':
+			if (config0.debug == 0b11) {
+				config0.debug = 0b10;
+			} else {
+				config0.debug = 0b11;
+			}
+			break;
+		case 'I':
+			config0.icesel = 1-config0.icesel;
+			break;
+		}
+	}
+}
+
+void watchdog() {
+	while (1) {
+		ansi.cls();
+		ansi.setCursor(0, 0);
+		heading("Watchdog Settings");
+		Serial.println();
+		Serial.print(" W) Watchdog .......................... ");
+		switch (config1.fwdten) {
+			case 0: 
+				Serial.println("Software Controlled"); 
+				break;
+			case 1: 
+				Serial.println("Always Enabled"); 
+				break;
+		}
+		Serial.print(" T) Timeout ........................... ");
+		if (ws_ps[config1.wdtps][0] == 'X') {
+			config1.wdtps = 0b10100;
+		}
+		Serial.println(ws_ps[config1.wdtps]);
+
+		Serial.print(" C) Clock Switching and Monitoring .... ");
+		switch (config1.fcksm) {
+			case 0:
+				Serial.println("Switch enabled, Monitor enabled");
+				break;
+			case 1:
+				Serial.println("Switch enabled, Monitor disabled");
+				break;
+			case 2:
+				Serial.println("Switch disabled, Monitor disabled");
+				break;
+			case 3:
+				Serial.println("Switch disabled, Monitor disabled");
+				break;
+		}
+		
+		subMenuFooter();
+		char c = getch();
+		switch(c) {
+			case 'X':
+				return;
+		case 'L':
+			if (confirm()) {
+				loadSettings();
+				Serial.println("Loaded.");
+			}
+			break;
+		case 'S':
+			if (confirm()) {
+				saveSettings();
+				Serial.println("Saved.");
+			}
+			break;
+		case 'R':
+			if (confirm()) {
+				Serial.println("Rebooting...");
+				Serial.flush();
+				reboot();
+			}
+			break;
+		case 'W':
+			config1.fwdten++;
+			break;
+		case 'T':
+			config1.wdtps++;
+			if (config1.wdtps > 0b10100) {
+				config1.wdtps = 0b00000;
+			}
+			break;
+		case 'C':
+			config1.fcksm++;
+			if (config1.fcksm == 3) {
+				config1.fcksm = 0;
+			}
+			break;
+		}
+	}
+}
+
+void usb() {
+	while (1) {
+		ansi.cls();
+		ansi.setCursor(0, 0);
+		heading("USB Settings");
+		Serial.println();
+		Serial.print(" P) USB PLL ........................... ");
+		Serial.println(config2.upllen ? "Disabled" : "Enabled");
+		Serial.print(" D) PLL Input Divider ................. ");
+		switch (config2.upllidiv) {
+			case 0:
+				Serial.println("/1");
+				break;
+			case 1:
+				Serial.println("/2");
+				break;
+			case 2:
+				Serial.println("/3");
+				break;
+			case 3:
+				Serial.println("/4");
+				break;
+			case 4:
+				Serial.println("/5");
+				break;
+			case 5:
+				Serial.println("/6");
+				break;
+			case 6:
+				Serial.println("/10");
+				break;
+			case 7:
+				Serial.println("/12");
+				break;
+		}
+
+		Serial.print(" V) VBUSON Pin Control ................ ");
+		Serial.println(config3.fvbusonio ? "Controlled by USB" : "Normal IO Port");
+		
+		Serial.print(" I) USBID Pin Control ................. ");
+		Serial.println(config3.fusbidio ? "Controlled by USB" : "Normal IO Port");
+
+		subMenuFooter();
+		char c = getch();
+		switch(c) {
+			case 'X':
+				return;
+		case 'L':
+			if (confirm()) {
+				loadSettings();
+				Serial.println("Loaded.");
+			}
+			break;
+		case 'S':
+			if (confirm()) {
+				saveSettings();
+				Serial.println("Saved.");
+			}
+			break;
+		case 'R':
+			if (confirm()) {
+				Serial.println("Rebooting...");
+				Serial.flush();
+				reboot();
+			}
+			break;
+		case 'P':
+			config2.upllen++;
+			break;
+		case 'D':
+			config2.upllidiv++;
+			break;
+		case 'V':
+			config3.fvbusonio++;
+			break;
+		case 'I':
+			config3.fusbidio++;
+			break;
+		}
+	}
+}
+
+void oscillator() {
+	while (1) {
+		ansi.cls();
+		ansi.setCursor(0, 0);
+		heading("Oscillator Settings");
+		Serial.println();
+		Serial.print(" O) Oscillator ........................ ");
+
+		switch (config1.fnosc) {
+			case 0:
+				Serial.println("Fast RC");
+				break;
+			case 1:
+				Serial.println("Fast RC with Div-N and PLL");
+				break;
+			case 2:
+				Serial.println("Primary");
+				break;
+			case 3:
+				Serial.println("Primary with PLL");
+				break;
+			case 4:
+				Serial.println("Secondary");
+				break;
+			case 5:
+				Serial.println("Low-Power RC");
+				break;
+			case 6:
+				Serial.println("Fast RC with Div-16");
+				break;
+			case 7:
+				Serial.println("Fast RC with Div-N");
+				break;
+		}
+		Serial.print(" 1) Primary Oscillator Mode ........... ");
+		switch (config1.poscmod) {
+			case 0:
+				Serial.println("External");
+				break;
+			case 1:
+				Serial.println("XT");
+				break;
+			case 2:
+				Serial.println("HS");
+				break;
+			case 3:
+				Serial.println("Disabled");
+				break;
+		}
+		Serial.print(" 2) Secondary Oscillator .............. ");
+		Serial.println(config1.fsoscen ? "Enabled" : "Disabled");
+		
+		Serial.print(" I) PLL Input Divider ................. ");
+		switch (config2.fpllidiv) {
+			case 0:
+				Serial.println("/1");
+				break;
+			case 1:
+				Serial.println("/2");
+				break;
+			case 2:
+				Serial.println("/3");
+				break;
+			case 3:
+				Serial.println("/4");
+				break;
+			case 4:
+				Serial.println("/5");
+				break;
+			case 5:
+				Serial.println("/6");
+				break;
+			case 6:
+				Serial.println("/10");
+				break;
+			case 7:
+				Serial.println("/12");
+				break;
+		}
+
+		Serial.print(" M) PLL Multiplier .................... ");
+		switch (config2.fpllmul) {
+			case 0:
+				Serial.println("x15");
+				break;
+			case 1:
+				Serial.println("x16");
+				break;
+			case 2:
+				Serial.println("x17");
+				break;
+			case 3:
+				Serial.println("x18");
+				break;
+			case 4:
+				Serial.println("x19");
+				break;
+			case 5:
+				Serial.println("x20");
+				break;
+			case 6:
+				Serial.println("x21");
+				break;
+			case 7:
+				Serial.println("x24");
+				break;
+		}
+
+		Serial.print(" D) PLL Output Divider ................ ");
+		switch (config2.fpllodiv) {
+			case 0:
+				Serial.println("/1");
+				break;
+			case 1:
+				Serial.println("/2");
+				break;
+			case 2:
+				Serial.println("/3");
+				break;
+			case 3:
+				Serial.println("/4");
+				break;
+			case 4:
+				Serial.println("/5");
+				break;
+			case 5:
+				Serial.println("/6");
+				break;
+			case 6:
+				Serial.println("/10");
+				break;
+			case 7:
+				Serial.println("/12");
+				break;
+		}
+
+		Serial.print(" P) Peripheral Bus Divider ............ /");
+		Serial.println(1 << config1.fpbdiv, DEC);
+
+		Serial.print(" C) Clock Output ...................... ");
+		Serial.println(config1.osciofnc ? "Disabled" : "Enabled");
+		
+		Serial.print(" T) Two Speed Startup ................. ");
+		Serial.println(config1.ieso ? "Enabled" : "Disabled");
+
+		subMenuFooter();
+		char c = getch();
+		switch(c) {
+			case 'X':
+				return;
+			case 'L':
+				if (confirm()) {
+					loadSettings();
+					Serial.println("Loaded.");
+				}
+				break;
+			case 'S':
+				if (confirm()) {
+					saveSettings();
+					Serial.println("Saved.");
+				}
+				break;
+			case 'R':
+				if (confirm()) {
+					Serial.println("Rebooting...");
+					Serial.flush();
+					reboot();
+				}
+				break;
+			case 'O':
+				config1.fnosc++;
+				break;
+			case '1':
+				config1.poscmod++;
+				break;
+			case '2':
+				config1.fsoscen++;
+				break;
+			case 'I':
+				config2.fpllidiv++;
+				break;
+			case 'M':
+				config2.fpllmul++;
+				break;
+			case 'D':
+				config2.fpllodiv++;
+				break;
+			case 'P':
+				config1.fpbdiv++;
+				break;
+			case 'C':
+				config1.osciofnc++;
+				break;
+			case 'T':
+				config1.ieso++;
+				break;
+		}
+	}
+}
+
+void peripherals() {
+	while (1) {
+		ansi.cls();
+		ansi.setCursor(0, 0);
+		heading("Peripheral Settings");
+		Serial.println();
+
+		Serial.print(" C) CAN I/O Pins ...................... ");
+		Serial.println(config3.fcanio ? "Default" : "Alternate");
+
+		Serial.print(" E) Ethernet I/O Pins ................. ");
+		Serial.println(config3.fethio ? "Default" : "Alternate");
+
+		Serial.print(" M) Ethernet MII ...................... ");
+		Serial.println(config3.fmiien ? "Enabled" : "Disabled");
+
+		Serial.print(" U) User ID ........................... ");
+		char tmp[10];
+		sprintf(tmp, "0x%04X", config3.userid);
+		Serial.println(tmp);
+
+		subMenuFooter();
+		char c = getch();
+		switch(c) {
+			case 'X':
+				return;
+			case 'L':
+				if (confirm()) {
+					loadSettings();
+					Serial.println("Loaded.");
+				}
+				break;
+			case 'S':
+				if (confirm()) {
+					saveSettings();
+					Serial.println("Saved.");
+				}
+				break;
+			case 'R':
+				if (confirm()) {
+					Serial.println("Rebooting...");
+					Serial.flush();
+					reboot();
+				}
+				break;
+			case 'C':
+				config3.fcanio++;
+				break;
+			case 'E':
+				config3.fethio++;
+				break;
+			case 'M':
+				config3.fmiien++;
+				break;
+			case 'U':
+				config3.userid = inputHex(config3.userid);
+				break;
+		}
+	}
+}
+
+int inputHex(int val) {
+	uint16_t hexval = val;
+	char tmp[50];
+	Serial.println();
+
+	while (1) {
+		sprintf(tmp, "\rEnter HEX value: 0x%04X", hexval);
+		Serial.print(tmp);
+		char c = getch();
+		switch (c) {
+			case '0':
+				hexval <<= 4;
+				break;
+			case '1':
+				hexval <<= 4;
+				hexval |= 1;
+				break;
+			case '2':
+				hexval <<= 4;
+				hexval |= 2;
+				break;
+			case '3':
+				hexval <<= 4;
+				hexval |= 3;
+				break;
+			case '4':
+				hexval <<= 4;
+				hexval |= 4;
+				break;
+			case '5':
+				hexval <<= 4;
+				hexval |= 5;
+				break;
+			case '6':
+				hexval <<= 4;
+				hexval |= 6;
+				break;
+			case '7':
+				hexval <<= 4;
+				hexval |= 7;
+				break;
+			case '8':
+				hexval <<= 4;
+				hexval |= 8;
+				break;
+			case '9':
+				hexval <<= 4;
+				hexval |= 9;
+				break;
+			case 'A':
+				hexval <<= 4;
+				hexval |= 10;
+				break;
+			case 'B':
+				hexval <<= 4;
+				hexval |= 11;
+				break;
+			case 'C':
+				hexval <<= 4;
+				hexval |= 12;
+				break;
+			case 'D':
+				hexval <<= 4;
+				hexval |= 13;
+				break;
+			case 'E':
+				hexval <<= 4;
+				hexval |= 14;
+				break;
+			case 'F':
+				hexval <<= 4;
+				hexval |= 15;
+				break;
+			case '\n':
+				return hexval;
+			case '\r':
+				return hexval;
+		}
+	}
+}
